d41153affb91a8524339063943749a9d6b7e5e64
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-04-29 23:02:50 +0000

    chore: added button variants

--- src/components/common/Button.component.tsx
+++ src/components/common/Button.component.tsx
@@ -2,31 +2,29 @@ import { ReactNode } from "@tanstack/react-router"
 
 
 interface ButtonProps {
-  variant?: 'Primary',
+  variant?: 'primary' |'outline',
   children: ReactNode,
   className?: string,
   onClick?: (...args: unknown[]) =>void,
 }
 
 export default function Button({
-    variant,
+    variant = 'primary',
     children,
     className,
     onClick,
   }: ButtonProps){
-  
-    switch(variant){
-    
-    default: 
-      return (
-        <button
-          className={`${className} h-[35px] rounded-md px-4 bg-primary text-white `}
-          onClick={onClick}
-        >
-          {children}
-        </button>
-      )
-        
-    }
-
+  return (
+    <>
+      <button
+        className={`${className} h-[35px] duration-150 rounded-md hover:shadow px-4 border-[1.5px] ` +
+          (variant === 'primary' ? 'bg-primary hover:opacity-70 text-white border-primary' : '') +
+          (variant === 'outline' ? 'bg-transparent hover:bg-primary hover:opacity-70 text-primary hover:text-white border-primary' : '')
+        }
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </>
+  )
 }
